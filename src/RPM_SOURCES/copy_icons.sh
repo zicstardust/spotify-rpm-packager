f30efbb280f4b5230881638fc3765b7368a8f1fb
Zic <git@zicstardust.com>
2026-01-08 15:30:10 -0400

    fix copy icons

--- src/RPM_SOURCES/copy_icons.sh
+++ src/RPM_SOURCES/copy_icons.sh
@@ -4,8 +4,8 @@ source_dir=$1
 destine_dir=$2
 
 for i in 16 22 24 32 48 64 128 256 512; do
-if [ -f "${source_dir}/spotify-linux-${i}.png" ]; then
-    mkdir -p ${destine_dir}/hicolor/${i}x${i}/apps
-    cp ${source_dir}/spotify-linux-${i}.png  ${destine_dir}/hicolor/${i}x${i}/apps/spotify.png
-fi
+    if [ -f "${source_dir}/spotify-linux-${i}.png" ]; then
+        mkdir -p ${destine_dir}/hicolor/${i}x${i}/apps
+        cp ${source_dir}/spotify-linux-${i}.png  ${destine_dir}/hicolor/${i}x${i}/apps/spotify.png
+    fi
 done
