4331165fe2e2e2311a74971843d38b3a267ad9c4
Zic <git@zicstardust.com>
2026-01-12 17:01:32 -0400

    more icons size

--- src/RPM_SOURCES/copy_icons.sh
+++ src/RPM_SOURCES/copy_icons.sh
@@ -4,13 +4,7 @@ source_dir=$1
 destine_dir=$2
 
 
-#Scalable icon
-#mkdir -p ${destine_dir}/hicolor/scalable/apps
-#curl -fSL "https://raw.githubusercontent.com/flathub/com.spotify.Client/refs/heads/master/com.spotify.Client.svg" -o ${destine_dir}/hicolor/scalable/apps/spotify-client.svg
-
-
-# Tradicional icons
-for i in 16 22 24 32 48 64 128 256 512; do
+for i in 8 16 22 24 32 36 44 48 64 72 96 128 144 150 192 256 310 512 1024; do
     if [ -f "${source_dir}/spotify-linux-${i}.png" ]; then
         mkdir -p ${destine_dir}/hicolor/${i}x${i}/apps
         cp ${source_dir}/spotify-linux-${i}.png  ${destine_dir}/hicolor/${i}x${i}/apps/spotify-client.png
