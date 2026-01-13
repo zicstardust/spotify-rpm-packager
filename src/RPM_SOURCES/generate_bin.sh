480dc997d1ace7b2cdc5fa00951ddef7ed21c677
Zic <git@zicstardust.com>
2026-01-12 22:01:06 -0400

    add spotify flags

--- src/RPM_SOURCES/generate_bin.sh
+++ src/RPM_SOURCES/generate_bin.sh
@@ -19,11 +19,37 @@ $EXPORT_FFMPEG
 # Disable hardware acceleration that causes black screen
 export SPOTIFY_CLEAN_CACHE=1
 
-# Flags to improve compatibility
+
+if [ ! -f \${HOME}/.config/spotify/spotify-flags.conf ]; then
+    mkdir -p \${HOME}/.config/spotify
+    cat > \${HOME}/.config/spotify/spotify-flags.conf <<"FLAGS"
+#If delete this file, it will be recreated with the default flags the next time you run Spotify.
+
+#Defaults Flags to improve Fedora/RHEL compatibility
+--disable-gpu-sandbox
+--disable-seccomp-filter-sandbox
+--no-zygote
+
+#Flags exemples:
+#--disable-gpu
+#--disable-gpu-shader-disk-cache
+#--force-device-scale-factor=<value>
+#--cache-path=<path>
+#--debug-level=OFF|MAX|<value range 0-8>
+#--uri=<spotify_uri>
+#--mu=<value>
+#--log-file=<path>
+#--show-console
+#--username=<username>
+#--password=<password>
+FLAGS
+
+fi
+
+mapfile -t FLAGS <<< "\$(grep -v '^#' "\${HOME}/.config/spotify/spotify-flags.conf")"
+
 exec /usr/share/spotify/spotify \\
-    --disable-gpu-sandbox \\
-    --disable-seccomp-filter-sandbox \\
-    --no-zygote \\
+    "\${FLAGS[@]}" \\
     "\$@"
 EXEC
 chmod +x ${destine_dir}/spotify
